574378ec9aa385f584c18c5046136077d95b97e2
mateusz.sadowski <195054@edu.p.lodz.pl>
2017-03-27 20:13:57 +0200

    Added mocking of NewsReaderFactory and created a test to check
    if NewsReaderFactory behaves correctly

--- src/test/java/lab3_2/NewsLoaderTest.java
+++ src/test/java/lab3_2/NewsLoaderTest.java
@@ -11,8 +11,13 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import edu.iis.mto.staticmock.Configuration;
 import edu.iis.mto.staticmock.ConfigurationLoader;
+import edu.iis.mto.staticmock.IncomingInfo;
+import edu.iis.mto.staticmock.IncomingNews;
+import edu.iis.mto.staticmock.NewsLoader;
+import edu.iis.mto.staticmock.NewsReaderFactory;
 import edu.iis.mto.staticmock.PublishableNews;
 import edu.iis.mto.staticmock.SubsciptionType;
+import edu.iis.mto.staticmock.reader.NewsReader;
 
 import static org.powermock.api.mockito.PowerMockito.*;
 
@@ -22,15 +27,20 @@ import org.mockito.internal.util.reflection.*;
 import static org.hamcrest.CoreMatchers.*;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest( ConfigurationLoader.class )
+@PrepareForTest( {
+	ConfigurationLoader.class,
+	NewsReaderFactory.class
+})
 public class NewsLoaderTest {
 	
-	public ConfigurationLoader testConfigurationLoader = null;
+	private ConfigurationLoader testConfigurationLoader = null;
+	private Configuration testConfiguration = null;
 	
 	@Before
 	public void setUpTest() {
 		initFakeConfigLoader();
 		initFakeConfig();
+		initFakeNewsReader();
 	}
 	
 	public void initFakeConfigLoader() {
@@ -40,9 +50,26 @@ public class NewsLoaderTest {
 	}
 	
 	public void initFakeConfig() {
-		Configuration testConfiguration = new Configuration();
-		Whitebox.setInternalState(testConfiguration, "readerType", "testNewsReader");
+		testConfiguration = new Configuration();
+		Whitebox.setInternalState(testConfiguration, "readerType", "test");
 		when(testConfigurationLoader.loadConfiguration()).thenReturn(testConfiguration);
+	}
+	
+	public void initFakeNewsReader() {
+		PowerMockito.mockStatic(NewsReaderFactory.class);
+		final IncomingNews news = new IncomingNews();
+		news.add(new IncomingInfo("pub", SubsciptionType.NONE));
+		news.add(new IncomingInfo("subA", SubsciptionType.A));
+		news.add(new IncomingInfo("subB", SubsciptionType.B));
+		news.add(new IncomingInfo("subC", SubsciptionType.C));
+		NewsReader testNewsReader = new NewsReader() {
+
+			@Override
+			public IncomingNews read() {
+				return news;
+			}
+		};
+		when(NewsReaderFactory.getReader("test")).thenReturn(testNewsReader);
 	}
 	
 	@Test
@@ -76,5 +103,13 @@ public class NewsLoaderTest {
 		List<String> testList = (List<String>) Whitebox.getInternalState(pn, "publicContent");
 		assertThat(testList.size(), is(not(equalTo(0))));
 		assertThat(testList.get(0), is(equalTo("pub")));
+	}
+	
+	@Test
+	public void testNewsReaderFactoryCorrectReaderUsedAndGetReaderCalledOnce() {
+		NewsLoader test = new NewsLoader();
+		test.loadNews();
+		PowerMockito.verifyStatic();
+		NewsReaderFactory.getReader("test");
 	}
 }
