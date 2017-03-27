2308ee7dc6f4a46f561d1fd0545313de6a386c41
mateusz.sadowski <195054@edu.p.lodz.pl>
2017-03-27 18:34:09 +0200

    Added dependencies from https://github.com/mto-lab/testisolation

---
+++ src/test/java/lab3_2/NewsLoaderTest.java
@@ -0,0 +1,15 @@
+package lab3_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import static org.powermock.api.mockito.PowerMockito.*;
+
+public class NewsLoaderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
