92bbdcc6067524cff4dfc87825e6342f0fa145ff
mateusz.sadowski <195054@edu.p.lodz.pl>
2017-03-27 19:20:38 +0200

    First approach to mocking created
    Based on
    https://github.com/mto-lab/testisolation/blob/master/src/test/java/edu/iis/mto/powermock/StaticSingletonTest.java

--- src/test/java/lab3_2/NewsLoaderTest.java
+++ src/test/java/lab3_2/NewsLoaderTest.java
@@ -2,14 +2,46 @@ package lab3_2;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import edu.iis.mto.staticmock.Configuration;
+import edu.iis.mto.staticmock.ConfigurationLoader;
+
 import static org.powermock.api.mockito.PowerMockito.*;
+import org.mockito.internal.util.reflection.*;
+import static org.hamcrest.CoreMatchers.*;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( ConfigurationLoader.class )
 public class NewsLoaderTest {
-
+	
+	public ConfigurationLoader testConfigurationLoader = null;
+	
+	@Before
+	public void setUpTest() {
+		initFakeConfigLoader();
+		initFakeConfig();
+	}
+	
+	public void initFakeConfigLoader() {
+		testConfigurationLoader = PowerMockito.mock(ConfigurationLoader.class);
+		PowerMockito.mockStatic(ConfigurationLoader.class);
+		PowerMockito.when(ConfigurationLoader.getInstance()).thenReturn(testConfigurationLoader);
+	}
+	
+	public void initFakeConfig() {
+		Configuration testConfiguration = new Configuration();
+		Whitebox.setInternalState(testConfiguration, "readerType", "testNewsReader");
+		when(testConfigurationLoader.loadConfiguration()).thenReturn(testConfiguration);
+	}
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		fail("Not yet implemented!");
 	}
-
 }
